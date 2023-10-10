eda86102d7ef1b6299846e79935bf77ce4d7ab3e
kfzjw008 <1174681078@qq.com>
2023-10-10 16:09:58 +0800

    数据注入业务:注入包检查业务。

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main() {
     mallocInit();//windows专用
     printf("First address of the virtual space= 0x%08X \n", MEMORY);
     printf("Online monitoring first address= 0x%08X \n", MONI_TAB_STR_ADRS);
-    outpDw(2, 256);
+    outpDw(2, 0x2E);
     printf("inpDw Test = %d ", inpDw(2));
     //哪吒架构第一行代码
     return 0;
